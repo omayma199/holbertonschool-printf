bca7cd18c743afa1bde0246609f8b2a1b02e334e
omayma199 <5752@holbertonstudents.com>
2022-11-11 18:58:50 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -1,48 +1,50 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
 /**
- * _printf - a typical printf
- * @format: is a character string
- * Return: the number of characters printed
+ * _printf - function like printf
+ * @format: the pointer of char
+ * Return: 1
  */
 int _printf(const char *format, ...)
 {
+	int count = 0, fun = 0, i = 0;
+	va_list args;
 
-	int i = 0, j = 0, a = 0;
-	va_list ap;
-
-	if (format == NULL || (strlen(format) == 1 && format[0] == '%'))
-	{
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	}
-	va_start(ap, format);
-	while (format && format[i])
+	va_start(args, format);
+	while (format[i])
 	{
 		if (format[i] != '%')
 		{
-			putchar(format[i]);
-			j++;
+			putchar(*(format + i));
+			count++;
 		}
 		if (format[i] == '%' && format[i + 1] != 'K' && format[i + 1] != '!')
 		{
-			a = get_printf(*(format + (i + 1)), ap);
-			if (a != 0)
-				j = j + a;
-			i = i + 2;
-			continue;
-			if (*(format + (i + 1)) == '\0')
+			fun = get_function(format[i + 1], args);
+			if (fun != 0)
+			{
+				count = count + fun;
+				i = i + 2;
+				continue;
+			}
+			if (format[i] == '\0')
 			{
 				putchar(format[i]);
-				j++;
+				count++;
 			}
-		}
-		else if ((format[i] == '%' && format[i + 1] == 'K') ||
-		 (format[i] == '%' && format[i + 1] == '!'))
-		{
-			putchar(format[i]);
-			j++;
+			else if ((format[i] == '%' && format[i + 1] == 'K')||(format[i] == '%' && format[i + 1] == '!')  )
+			{
+				putchar(format[i]);
+				count++;
+			}
+			
 		}
 		i++;
 	}
-	va_end(ap);
-	return (j);
+	va_end(args);
+	return (count);
 }
