2de217da922b0d7b540df2f81ae711ca97dc8b59
omayma199 <5752@holbertonstudents.com>
2022-11-11 12:08:23 +0100

    printf

---
+++ getfunction.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * get_function - select function for conversion char
+ * @s: char to check
+ * Return: pointer to function
+ */
+int get_function(const char s, va_list args)
+{
+    int i = 0;
+    int j = 0;
+    
+    print_t print[] = {
+        {'c', printcharacter},
+		{'s', printstring},
+		{'d', printint},
+		{'i', printint},
+        {'%', printpercent}};
+        while (print[i].c != 0)
+        {
+            if (print[i].c == s)
+            {
+                j = j + print[i].f(args);
+                return (j);
+            }
+            i++;
+        }
+
+}
