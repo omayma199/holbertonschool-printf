8ded2bb9789ffc26dd3235daf34933ccaf91e877
omayma199 <5752@holbertonstudents.com>
2022-11-11 15:33:00 +0100

    headfile

--- main.h
+++ main.h
@@ -5,21 +5,16 @@
 #include <unistd.h>
 #include <string.h>
 
-
-
 typedef struct print
 {
 	char c;
 	int (*f)(va_list args);
 } print_t;
 int _putchar(char c);
-int get_function(const char c, va_list li);
-int _printf(const char *format, ...);
-
-
+int get_function(const char s, va_list args);
 
 int printstring(va_list args);
 int printcharacter(va_list args);
-int printint(int n);
+int printint(va_list args);
 int printpercent(va_list args);
 #endif
