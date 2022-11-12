9e2c93ec612a26480daa21245cc69c62e652cf34
omayma199 <5752@holbertonstudents.com>
2022-11-12 01:21:51 +0100

    printf

--- getfunction.c
+++ getfunction.c
@@ -12,7 +12,7 @@ int get_function(const char s, va_list arg)
 
     print_t type[] = {
         {'c', printcharacter},
-        {'s', funct_string},
+        {'s', printstring},
         {'d', printint},
         {'i', printint},
         {'%', printpercent}};
